eb74223aaee164018b38a225e0babb72d3abb46a
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2025-09-26 23:01:30 +0200

    feat: preloader component

--- www/js/index.js
+++ www/js/index.js
@@ -31,3 +31,12 @@
         UnicornStudio.init();
       }
     });
+
+  window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+      preloader.style.opacity = '0';
+      preloader.style.transition = 'opacity 0.5s ease';
+      setTimeout(() => preloader.style.display = 'none', 500);
+    }
+  });
